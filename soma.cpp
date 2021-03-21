bcf3042173c480cc03a8a7d86426f7c6fe516420
matheusramires <80134011+matheusramires@users.noreply.github.com>
2021-03-21 15:48:02 -0300

    Add files via upload

---
+++ soma.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+main(){
+	
+	int numero1, numero2, soma;
+	
+	std::cout << "Digite um numero: ";
+	std::cin >> numero1;
+	
+	std::cout << "Digite o segundo numero: ";
+	std::cin >> numero2;
+	
+	soma = numero1 + numero2;
+	
+	std::cout << "Soma eh:" <<soma <<std::endl;
+}
